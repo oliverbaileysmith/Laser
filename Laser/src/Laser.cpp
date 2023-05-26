bde0f128c7210955ac88c409bbb07876973a0f13
Oliver Bailey Smith <smitholiverbailey@gmail.com>
2023-05-26 13:30:14 -0400

    Add basic camera and rays

--- Laser/src/Laser.cpp
+++ Laser/src/Laser.cpp
@@ -39,14 +39,26 @@ int main()
 
 	// OpenCL kernel source
 	const char* src =
-		"__kernel void testKernel(__global float3* output, int width, int height)"
+		"struct Ray"
+		"{"
+		"float3 m_Origin;"
+		"float3 m_Direction;"
+		"};"
+		"__kernel void Laser(__global float3* output, int imageWidth, int imageHeight, float aspectRatio, float viewportWidth, float viewportHeight, float focalLength, float3 cameraOrigin, float3 lowerLeftCorner)"
 		"{"
 		"const int workItemID = get_global_id(0);"
-		"int x = workItemID % width;"
-		"int y = workItemID / width;"
-		"float fx = (float)x / (float)width;"
-		"float fy = (float)y / (float)height;"
-		"output[workItemID] = (float3)(fx, fy, 0.0f);"
+		"int x = workItemID % imageWidth;"
+		"int y = workItemID / imageWidth;"
+		"float fx = (float)x / (float)(imageWidth - 1);"
+		"float fy = (float)y / (float)(imageHeight - 1);"
+		"struct Ray ray;"
+		"ray.m_Origin = cameraOrigin;"
+		"ray.m_Direction = lowerLeftCorner;"
+		"ray.m_Direction.x += fx * viewportWidth;"
+		"ray.m_Direction.y += fy * viewportHeight;"
+		"ray.m_Direction -= cameraOrigin;"
+		"ray.m_Direction = normalize(ray.m_Direction);"
+		"output[workItemID] = fabs(ray.m_Direction);"
 		"}";
 
 	// OpenCL program
@@ -60,11 +72,21 @@ int main()
 	}
 
 	// OpenCL kernel
-	cl::Kernel kernel(program, "testKernel");
+	cl::Kernel kernel(program, "Laser");
 
 	// Host data
 	const int imageWidth = 1280;
 	const int imageHeight = 720;
+	const float aspectRatio = (float)imageWidth / (float)imageHeight;
+	float viewportHeight = 2.0f;
+	float viewportWidth = viewportHeight * aspectRatio;
+	float focalLength = 1.0f;
+	cl_float3 cameraOrigin = { 0.0f,0.0f,0.0f };
+	cl_float3 lowerLeftCorner = cameraOrigin;
+	lowerLeftCorner.x -= viewportWidth / 2.0f;
+	lowerLeftCorner.y -= viewportHeight / 2.0f;
+	lowerLeftCorner.z -= focalLength;
+
 	cl_float3* cpuOutput = new cl_float3[imageWidth * imageHeight];
 
 	// OpenCL device data
@@ -74,6 +96,12 @@ int main()
 	kernel.setArg(0, clOutput);
 	kernel.setArg(1, imageWidth);
 	kernel.setArg(2, imageHeight);
+	kernel.setArg(3, aspectRatio);
+	kernel.setArg(4, viewportWidth);
+	kernel.setArg(5, viewportHeight);
+	kernel.setArg(6, focalLength);
+	kernel.setArg(7, cameraOrigin);
+	kernel.setArg(8, lowerLeftCorner);
 
 	// OpenCL command queue
 	cl::CommandQueue queue(context, device);
@@ -98,9 +126,9 @@ int main()
 	for (int i = 0; i < imageWidth * imageHeight; i++)
 	{
 		outputFile
-			<< (int)(cpuOutput[i].x * 256) << " "
-			<< 255 - (int)(cpuOutput[i].y * 256) << " "
-			<< (int)(cpuOutput[i].z * 256) << std::endl;
+			<< (int)(cpuOutput[i].x * 255) << " "
+			<< (int)(cpuOutput[i].y * 255) << " "
+			<< (int)(cpuOutput[i].z * 255) << std::endl;
 	}
 	outputFile.close();
 

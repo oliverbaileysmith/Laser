a2f11b7a19274b09d2f8543f5e25eb37bc312e8b
Oliver Bailey Smith <smitholiverbailey@gmail.com>
2023-05-30 12:09:55 -0400

    Add render stats and improve console output

--- Laser/src/Laser.cl
+++ Laser/src/Laser.cl
@@ -11,6 +11,14 @@ struct Triangle
 	float3 v2;
 };
 
+struct RenderStats
+{
+	unsigned int n_PrimaryRays;
+	unsigned int n_RayTriangleTests;
+	unsigned int n_RayTriangleIsects;
+	float renderTime;
+};
+
 bool intersectTriangle(struct Ray* ray, struct Triangle* triangle, float* t, float3* norm)
 {
 	// calculate triangle/plane normal (this can be precalculated or included in geometry in future)
@@ -25,7 +33,7 @@ bool intersectTriangle(struct Ray* ray, struct Triangle* triangle, float* t, flo
         return false;
 
 	*t = - (dot(*norm, ray->orig) + D) / normDotRayDir;
-	if (*t < 0.00001f) // test if hit point is behind or "inside" camera
+	if (*t < 0.0f) // test if hit point is behind or "inside" camera
 		return false;
 
 	float3 p = ray->orig + *t * ray->dir;
@@ -58,7 +66,8 @@ __kernel void Laser(__global float3* output,
 	int imageWidth, int imageHeight, float aspectRatio,
 	float viewportWidth, float viewportHeight, float focalLength,
 	float3 cameraOrigin, float3 upperLeftCorner,
-	__global struct Triangle* triangle)
+	__global struct Triangle* triangle,
+	__global struct RenderStats* stats)
 {
 	// calculate pixel coordinates
 	const int workItemID = get_global_id(0);
@@ -68,6 +77,7 @@ __kernel void Laser(__global float3* output,
 	float fy = (float)y / (float)(imageHeight - 1);
 
 	// generate ray
+	atomic_inc(&(stats->n_PrimaryRays));
 	struct Ray ray;
 	ray.orig = cameraOrigin;
 	ray.dir = upperLeftCorner;
@@ -80,8 +90,12 @@ __kernel void Laser(__global float3* output,
 	float3 norm = (float3)(0.0f,0.0f,0.0f);
 	struct Triangle tri = *triangle;
 
+	atomic_inc(&(stats->n_RayTriangleTests));
 	if (intersectTriangle(&ray, &tri, &t, &norm))
-		output[workItemID] = (float3)(1.0f,0.0f,0.0f);
+	{
+		atomic_inc(&(stats->n_RayTriangleIsects));
+		output[workItemID] = (float3)(1.0f, 0.0f, 0.0f);
+	}
 	else
-		output[workItemID] = (float3)(0.0f,0.0f,0.0f);
+		output[workItemID] = (float3)(0.0f, 0.0f, 0.0f);
 }
--- Laser/src/Laser.cpp
+++ Laser/src/Laser.cpp
@@ -11,6 +11,14 @@ struct Triangle
 	cl_float3 v2;
 };
 
+struct RenderStats
+{
+	cl_uint n_PrimaryRays = 0;
+	cl_uint n_RayTriangleTests = 0;
+	cl_uint n_RayTriangleIsects = 0;
+	cl_float renderTime = 0.0f; // Time in seconds
+};
+
 int main()
 {
 	// OpenCL platform
@@ -40,6 +48,7 @@ int main()
 	// Print OpenCL platform and device info
 	std::cout << "OpenCL platform: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
 	std::cout << "OpenCL device: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+	std::cout << "OpenCL version: " << device.getInfo<CL_DEVICE_VERSION>() << std::endl;
 
 	// OpenCL context
 	cl::Context context(device);
@@ -70,12 +79,12 @@ int main()
 	cl::Kernel kernel(program, "Laser");
 
 	// Host data
-	const int imageWidth = 1280;
-	const int imageHeight = 720;
-	const float aspectRatio = (float)imageWidth / (float)imageHeight;
-	float viewportHeight = 2.0f;
-	float viewportWidth = viewportHeight * aspectRatio;
-	float focalLength = 1.0f;
+	const cl_int imageWidth = 1280;
+	const cl_int imageHeight = 720;
+	const cl_float aspectRatio = (cl_float)imageWidth / (cl_float)imageHeight;
+	cl_float viewportHeight = 2.0f;
+	cl_float viewportWidth = viewportHeight * aspectRatio;
+	cl_float focalLength = 1.0f;
 	cl_float3 cameraOrigin = { 0.0f,0.0f,0.0f };
 	cl_float3 upperLeftCorner = cameraOrigin;
 	upperLeftCorner.x -= viewportWidth / 2.0f;
@@ -89,9 +98,12 @@ int main()
 	tri.v1 = {  0.5f, -0.5f, -1.0f };
 	tri.v2 = {  0.0f,  0.5f, -1.0f };
 
+	RenderStats stats;
+
 	// OpenCL device data
 	cl::Buffer clOutput(context, CL_MEM_WRITE_ONLY, imageWidth * imageHeight * sizeof(cl_float3));
 	cl::Buffer clTriangle(context, CL_MEM_READ_ONLY, sizeof(tri));
+	cl::Buffer clStats(context, CL_MEM_READ_WRITE, sizeof(RenderStats));
 
 	// OpenCL kernel arguments
 	kernel.setArg(0, clOutput);
@@ -104,6 +116,7 @@ int main()
 	kernel.setArg(7, cameraOrigin);
 	kernel.setArg(8, upperLeftCorner);
 	kernel.setArg(9, clTriangle);
+	kernel.setArg(10, clStats);
 
 	// OpenCL command queue
 	cl::CommandQueue queue(context, device);
@@ -112,15 +125,29 @@ int main()
 	std::size_t globalWorkSize = imageWidth * imageHeight;
 	std::size_t localWorkSize = 64;
 
+	clock_t timeStart = clock();
+
 	// Queue kernel execution and read result from device buffer
 	queue.enqueueWriteBuffer(clTriangle, CL_TRUE, 0, sizeof(tri), &tri);
 	queue.enqueueNDRangeKernel(kernel, NULL, globalWorkSize, localWorkSize);
 	queue.enqueueReadBuffer(clOutput, CL_TRUE, 0, imageWidth * imageHeight * sizeof(cl_float3), cpuOutput);
+	queue.enqueueReadBuffer(clStats, CL_TRUE, 0, sizeof(RenderStats), &stats);
+
+	clock_t timeEnd = clock();
+	stats.renderTime = (cl_float)(timeEnd - timeStart) / CLOCKS_PER_SEC;
+
+	// Write render stats to console
+	std::cout << std::endl;
+	std::cout << "Render time:                " << stats.renderTime << " seconds" << std::endl;
+	std::cout << "Primary rays:               " << stats.n_PrimaryRays << std::endl;
+	std::cout << "Ray-triangle tests:         " << stats.n_RayTriangleTests << std::endl;
+	std::cout << "Ray-triangle intersections: " << stats.n_RayTriangleIsects << std::endl;
 
 	// Write image to .ppm file
-	std::cout << "Writing to file...";
+	std::string outputPath = "output.ppm";
+	std::cout << std::endl << "Writing to file \"" << outputPath << "\"..." << std::endl;
 	std::ofstream outputFile;
-	outputFile.open("output.ppm");
+	outputFile.open(outputPath);
 
 	outputFile << "P3" << std::endl;
 	outputFile << imageWidth << " " << imageHeight << std::endl;
@@ -129,11 +156,12 @@ int main()
 	for (int i = 0; i < imageWidth * imageHeight; i++)
 	{
 		outputFile
-			<< (int)(cpuOutput[i].x * 255) << " "
-			<< (int)(cpuOutput[i].y * 255) << " "
-			<< (int)(cpuOutput[i].z * 255) << std::endl;
+			<< (int32_t)(cpuOutput[i].x * 255) << " "
+			<< (int32_t)(cpuOutput[i].y * 255) << " "
+			<< (int32_t)(cpuOutput[i].z * 255) << std::endl;
 	}
 	outputFile.close();
+	std::cout << "Finished writing to file." << std::endl;
 
 	delete[] cpuOutput;
 }

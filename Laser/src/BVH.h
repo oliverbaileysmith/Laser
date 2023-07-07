12a424f4b7dfc146644f24f1f45f986930955f58
Oliver Bailey Smith <smitholiverbailey@gmail.com>
2023-07-07 10:44:10 -0400

    Remove Octree

--- Laser/src/BVH.h
+++ Laser/src/BVH.h
@@ -10,54 +10,19 @@ class BVH
 {
 public:
 	BVH(const std::vector<cl_float3>& vertices, const std::vector<Triangle>& triangles, const std::vector<glm::mat4>& transforms);
-
-	Bounds GetSceneBounds() const;
-
-private:
-	Bounds GetTriangleBounds(cl_uint index) const;
-	cl_float3 GetCentroid(const Bounds& triangleBounds) const;
+	~BVH();
 
 private:
-	// Octree node
-	struct OctreeNode
+	// BVH node for building
+	struct BVHBuildNode
 	{
-	public:
-		OctreeNode* Children[8];
-		Bounds NodeBounds;
-		cl_bool IsLeaf;
-		std::vector<cl_uint> Triangles; // indices into scene's triangles array
-		uint32_t Depth = 0;
-
-		OctreeNode(const Bounds& bounds);
-		~OctreeNode();
-	};
-
-	// Octree used to build BVH by spatially grouping triangles
-	class Octree
-	{
-	public:
-		Octree(BVH* bvh, cl_uint maxDepth = 32);
-		~Octree();
-
-		void Insert(cl_uint triangle);
-		void Build();
-
-	private:
-		Bounds GetChildBounds(cl_char index, const cl_float3& nodeCentroid, const Bounds& bounds) const;
-		Bounds GetTriangleBounds(cl_uint index) const;
-		cl_float3 GetCentroid(const Bounds& triangleBounds) const;
-
-		void Insert(OctreeNode* node, cl_uint triangle, const Bounds& bounds, uint32_t depth);
-
-	private:
-		OctreeNode* m_Root;
-		BVH* m_BVH;
-		Bounds m_Bounds;
-		cl_uint m_MaxDepth;
+		Bounds Bounds;
+		BVHBuildNode* Children[2];
+		cl_uint FirstTriangle;
+		cl_uint nTriangles;
 	};
 
 	// Scene data
-	Bounds m_Bounds;
 	std::vector <cl_float3> m_Vertices;
 	std::vector<Triangle> m_Triangles;
 	std::vector<glm::mat4> m_Transforms;
